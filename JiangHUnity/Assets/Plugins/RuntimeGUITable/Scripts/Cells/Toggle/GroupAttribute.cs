5bfe3f647db7082bdd490f217411dad99c2f7502
sericaer <sericaer@outlook.com>
2021-09-13 22:49:29 +0800

    Add GUI Table

---
+++ JiangHUnity/Assets/Plugins/RuntimeGUITable/Scripts/Cells/Toggle/GroupAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityUITable
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class GroupAttribute : Attribute
+    {
+    }
+}
